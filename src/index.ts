0dac87602efeba137a89641251963246d7ee57e7
leandro <martinezleandro2004@gmail.com>
2022-06-29 15:36:49 -0300

    server running

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+app.get('/', (_req ,res )=>{
+  console.log('Hello');
+  res.send('Hello World!')  
+})
+
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`)
+)
